b8fb8c8082fae51685b1cdeb656cd595c9f6c9e9
danilogazzoli <daniloresende@yahoo.com>
2020-02-15 16:04:47 -0300

    exemplos com post

--- aula3/urls.py
+++ aula3/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import index, setacookie, redireciona
+from . import views
+
 
 app_name = "aula3"
 
@@ -7,4 +9,8 @@ urlpatterns = [
     path('', index),
     path('cookie', setacookie),
     path('uol', redireciona),
+    path('<int:code>', views.show_code),
+    path('cats/<int:code>',views.go_cats),
+    path('get/', views.show_get_values),
+    path('post/', views.show_post_values)
 ]
--- aula3/views.py
+++ aula3/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
@@ -16,3 +17,40 @@ def setacookie(request):
 
 def redireciona(request):
     return HttpResponseRedirect('https://uol.com.br')
+
+def show_code(request, code):
+    html = f"<h1>O código é {code} </h1>"
+    response = HttpResponse(html)
+    return response
+
+def go_cats(request, code):
+    html = f"<h1>O código do cats {code} </h1>"
+    return HttpResponseRedirect(f'https://http.cat/{code}')
+
+def show_get_values(request):
+    #import ipdb; ipdb.set_trace()
+    nome = request.GET.get("nome", None)
+    if nome is None:
+        html = '<h1> Bem vindo usuário anônimo </h1>'
+    else:
+        html = f"<h1>Bem vindo {nome} </h1>"
+    return HttpResponse(html)
+
+@csrf_exempt
+def show_post_values(request):
+    head = ""
+    if request.method == 'POST':
+        nome = request.POST.get("nome")
+        sobrenome = request.POST.get("sobrenome")
+        head += f"<h1> bem vindo {nome} {sobrenome} </h1>"
+    html = """
+<form method=POST>
+  <label for="nome">Nome:</label><br>
+  <input type="text" id="nome" name="nome" value="John"><br>
+  <label for="sobrenome">Sobrenome:</label><br>
+  <input type="text" id="sobrenome" name="sobrenome" value="Doe"><br><br>
+  <input type="submit" value="Enviar">
+</form> 
+    """
+    html_to_response = head + html
+    return HttpResponse(html_to_response)
